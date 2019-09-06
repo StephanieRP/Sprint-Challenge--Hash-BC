ee53e2b207cae682c220027bed9191f8850a5211
Stephanie Patton <stephaniepatton@stephanies-air.attlocal.net>
2019-09-06 10:45:28 -0500

    start ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,7 +13,18 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
-    return None
+    if limit == None:
+        return None
+    # Declare count property
+    count = 0
+    # Put array elements into two hash tables, using for loop,
+    for i in range(0, len(weights)):
+        hash_table_insert(ht, count, weights[i])
+        count += 1
+
+    # If the count is one, return None
+    if count == 1 or count == 0:
+        return None
 
 
 def print_answer(answer):
